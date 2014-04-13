48a79290e0f3c5cb9df97e48127853c5251edc53
Ashish R Patel <patelashishpatel@gmail.com>
2014-04-12 21:44:39 -0600

    basic functional webservice ready

---
+++ classes/BrandingWebservice.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * 	This class handles request for university branding and returns appropriate response
+ * 	Author: Ashish R Patel
+ */
+Class BrandingWebservice {
+
+    // properties
+    private $_config = "";
+    private $_universities = array(
+        "engage" => array(
+            "name" => "Engage University is an online learning resource for DotNetNuke, offering web-based, self-paced DNN training.",
+            "logo" => "http://www.engageuniversity.com/Portals/0/Skins/logo.gif",
+            "url" => "http://www.engageuniversity.com/"
+        )
+    );
+    private $_httpResponseCodes = array(
+        200 => 'OK',
+        404 => 'Invalid Request',
+        405 => 'Method Not Allowed'
+    );
+
+    // methods
+    public function __construct($config) {
+        $this->_config = $config;
+    }
+
+    public function deliverResponse() {
+        // default response
+        $response = null;
+
+        // set content type header
+        header("Content-Type:application/json");
+
+        // check if the request method is allowed or not
+        $status = $this->_validateRequestMethod();
+        
+        // set HTTP response status header
+        header("HTTP/1.1 $status {$this->_httpResponseCodes[$status]}");
+
+        // now lets process the request
+        $response = $this->processRequest();
+
+        // encode response to json
+        echo json_encode($response);
+    }
+
+    protected function processRequest() {
+        
+        return (isset($this->_universities[$_GET['name']])) ? $this->_universities[$_GET['name']] : null;
+    }
+
+    private function _validateRequestMethod() {
+        if (in_array($_SERVER['REQUEST_METHOD'], $this->_config['allowed_methods'])) {
+            $status = 200;  // ok
+        } else {
+            $status = 405;  // request method not allowed
+        }
+        
+        return $status;
+    }
+
+}
--- index.php
+++ index.php
@@ -2,3 +2,11 @@
 
 // include the class file
 include("classes/BrandingWebservice.php");
+
+// instantiate brandingWebservice class
+$brandingWebserviceObj = new BrandingWebservice(array(
+	"allowed_methods" => array("GET")
+));
+
+// deliver response
+$brandingWebserviceObj->deliverResponse();
